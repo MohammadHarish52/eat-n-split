a0520a4da275c62f85e05e2c61289c2f7cdc9f45
tachyonok <tachyonparticle123@gmail.com>
2023-10-23 22:56:53 +0530

    key

--- src/App.js
+++ src/App.js
@@ -294,6 +294,7 @@ function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
